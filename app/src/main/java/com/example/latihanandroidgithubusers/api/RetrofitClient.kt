ffd383900e3b695f93dbf7c443988a47432d2330
hariadhari <muhamad.adhaari@kompas.com>
2022-12-02 14:09:23 +0700

    using okhttplogginginterceptor

--- app/src/main/java/com/example/latihanandroidgithubusers/api/RetrofitClient.kt
+++ app/src/main/java/com/example/latihanandroidgithubusers/api/RetrofitClient.kt
@@ -1,17 +1,32 @@
 package com.example.latihanandroidgithubusers.api
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 
 /* This is retrofit singleton */
 object RetrofitClient {
     private const val BASE_URL = "https://api.github.com/"
 
+    private fun provideOkHttpClient(): OkHttpClient {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor.apply {
+                httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+            })
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
+
     private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(provideOkHttpClient())
             .build()
     }
 
